6da8e3d1f25350df1d008107ee0450f677123282
Kjbhargava <karanjbhargava@gmail.com>
2019-10-14 17:40:59 -0700

    using js date to format to string for to do list

--- app.js
+++ app.js
@@ -9,38 +9,20 @@ app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
 
-    var day = "";
-
     var today = new Date();
-    var currentDay = today.getDay();
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
 
-    res.render("list", {kindOfDay: day});
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month:'long',
+        year: 'numeric'
+    };
 
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 app.listen(3000, function(){
