c3c2e8d04e050107311e4794b6a4041be5060c43
Kjbhargava <karanjbhargava@gmail.com>
2019-10-16 20:09:01 -0700

    finished basic ejs templating

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ['Buy Food', 'Cook Food', 'Eat Food'];
-let workItems = [];
+console.log(date);
+
+const items = ['Buy Food', 'Cook Food', 'Eat Food'];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -15,16 +18,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month:'long',
-        year: 'numeric'
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+let day = date.getDate();
 
     res.render("list", {
         listTitle: day,
@@ -33,24 +27,35 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    console.log(req.body);
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
-    items.push(item);
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 
-    res.redirect("/");
 });
 
 app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems});
 });
 
 app.post("/work", function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
 });
 
 app.listen(3000, function(){
